fab724b5db94df7d2e9d8b8c5a1da1cca5e646a5
Chuu Htet Naing <chuuhtetnaing@gmail.com>
2017-04-22 12:07:10 +0700

    Added Background and News Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :backgrounds
+  resources :news
+  mount Ckeditor::Engine => '/ckeditor'
   root 'emuseum#home'
   get 'emuseum/home'
 
-  get 'emuseum/background'
-
-  get 'emuseum/news'
+  get 'emuseum/news' => 'news#news'
+  get 'emuseum/background' => 'backgrounds#background' 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
