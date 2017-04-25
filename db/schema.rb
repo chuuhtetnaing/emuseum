9d0b1490d0762193a98fdb6bfc8183c60948cedc
Chuu Htet Naing <chuuhtetnaing@gmail.com>
2017-04-26 01:37:48 +0700

    Finish for index page view without search

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425171813) do
+ActiveRecord::Schema.define(version: 20170425181755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -156,6 +156,7 @@ ActiveRecord::Schema.define(version: 20170425171813) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.text     "detail"
   end
 
   create_table "unblock_emails", force: :cascade do |t|
