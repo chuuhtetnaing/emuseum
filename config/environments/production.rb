8a57bd0e2b710e1daebcfe2159de1bc84f71a732
Chuu Htet Naing <chuuhtetnaing@gmail.com>
2017-04-26 02:44:04 +0700

    fix for heroku error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,6 @@ Rails.application.configure do
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
   }
-}
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
