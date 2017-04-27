68d1b51d90bad4ec46ca4ca8b7fc7b2ca41b5010
Chuu Htet Naing <chuuhtetnaing@gmail.com>
2017-04-28 01:23:33 +0700

    Added Search and finished

--- app/controllers/emuseum_controller.rb
+++ app/controllers/emuseum_controller.rb
@@ -14,6 +14,58 @@ class EmuseumController < ApplicationController
   def index
   	@i = 0
   	@j = 0
-  	@museumowner = MuseumOwner.all.paginate(:page => params[:page], :per_page => 6)
+    @division = Division.all
+    if(params[:commit] == "Search" && !params[:museum_type].empty? | !params[:division_id].empty? | !params[:exhibition_type].empty? | !params[:search].empty?)
+       @museum_type = params[:museum_type]
+       @division_id = params[:division_id]
+       @exhibition_type = params[:exhibition_type]
+       @search = params[:search].downcase
+       @museumowner = MuseumOwner.all
+       @mo_sub = Subscription.all
+       
+       case
+        when @exhibition_type.empty? && @museum_type.empty? && @search.empty?
+          @mo_sub = @mo_sub.where(division_id: @division_id)
+        when @division_id.empty? && @exhibition_type.empty? && @museum_type.empty?
+          @mo_sub = @mo_sub.where("lower(org_name_eng) LIKE ? OR lower(detail) LIKE ?" , "%#{@search}%", "%#{@search}%")
+        when @division_id.empty? && @exhibition_type.empty? && @search.empty?
+          @mo_sub = @mo_sub.where("#{@museum_type}": true)
+        when @division_id.empty? && @museum_type.empty? && @search.empty?
+          @mo_sub = @mo_sub.where("#{@exhibition_type}": true)
+        when @division_id.empty? && @exhibition_type.empty?
+          @mo_sub = @mo_sub.where("lower(org_name_eng) LIKE ? OR lower(detail) LIKE ?" , "%#{@search}%", "%#{@search}%")
+          @mo_sub = @mo_sub.where("#{@museum_type}")
+        when @division_id.empty? && @museum_type.empty?
+          @mo_sub = @mo_sub.where("lower(org_name_eng) LIKE ? OR lower(detail) LIKE ?" , "%#{@search}%", "%#{@search}%")
+          @mo_sub = @mo_sub.where("#{@exhibition_type}": true)
+        when @division_id.empty? && @search.empty?
+          @mo_sub = @mo_sub.where("#{@museum_type}": true, "#{@exhibition_type}": true)
+        when @exhibition_type.empty? && @museum_type.empty?
+          @mo_sub = @mo_sub.where("lower(org_name_eng) LIKE ? OR lower(detail) LIKE ?" , "%#{@search}%", "%#{@search}%")
+          @mo_sub = @mo_sub.where(division_id: @division_id)
+        when @exhibition_type.empty? && @search.empty?
+          @mo_sub = @mo_sub.where("#{@museum_type}": true, division_id: @division_id)
+        when @museum_type.empty? && @search.empty?
+          @mo_sub = @mo_sub.where("#{@exhibition_type}": true, division_id: @division_id)
+        when @division_id.empty?
+          @mo_sub = @mo_sub.where("lower(org_name_eng) LIKE ? OR lower(detail) LIKE ?" , "%#{@search}%", "%#{@search}%")
+          @mo_sub = @mo_sub.where("#{@museum_type}": true, "#{@exhibition_type}": true)
+        when @exhibition_type.empty?
+          @mo_sub = @mo_sub.where("lower(org_name_eng) LIKE ? OR lower(detail) LIKE ?" , "%#{@search}%", "%#{@search}%")
+          @mo_sub = @mo_sub.where("#{@museum_type}": true, "#{@exhibition_type}": true, division_id: @division_id)
+        when @museum_type.empty?
+          @mo_sub = @mo_sub.where("lower(org_name_eng) LIKE ? OR lower(detail) LIKE ?" , "%#{@search}%", "%#{@search}%")
+          @mo_sub = @mo_sub.where("#{@museum_type}": true, "#{@exhibition_type}": true, division_id: @division_id)
+        when @search.empty?
+          @mo_sub = @mo_sub.where("lower(org_name_eng) LIKE ? OR lower(detail) LIKE ?" , "%#{@search}%", "%#{@search}%")
+          @mo_sub = @mo_sub.where("#{@museum_type}": true, "#{@exhibition_type}": true)
+        end
+        @mo_sub = @museumowner.where(subscription_id: @mo_sub.ids)
+       @museumowner = @mo_sub.paginate(:page => params[:page], :per_page => 6)
+
+    else
+        @museumowner = MuseumOwner.all.paginate(:page => params[:page], :per_page => 6)
+    end
+  	
   end
 end
