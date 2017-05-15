dbc48daf86795ace3217aa460c6e88b93244eefe
Chuu Htet Naing <chuuhtetnaing@gmail.com>
2017-05-15 19:01:09 +0700

    fix for s3 4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,16 @@ Rails.application.configure do
   
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.paperclip_defaults = {
+  storage: :s3,
+  s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
+  s3_credentials: {
+    bucket: ENV('S3_BUCKET_NAME'),
+    access_key_id: ENV('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV('AWS_REGION'),
+  }
+}
   # Show full error reports.
   config.consider_all_requests_local = true
 
